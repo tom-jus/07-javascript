a7403c92bf0626f9d114a7f6aece81c26c130eaf
tom-jus <101270851+tom-jus@users.noreply.github.com>
2022-09-01 16:26:08 -0300

    Add files via upload

---
+++ conjuntos.js
@@ -0,0 +1,12 @@
+
+const family = [ 
+    "Patricia", 
+    "Tomas", 
+    "Agustin", 
+    "Ricardo", 
+]
+
+const familtySet = new Set(family)
+familtySet.add("Tomas")
+familtySet.add("Javascript")
+console.log(familtySet)
